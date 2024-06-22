3eea8d0e736ea7476001dd6f67e094e00fc81c98
Halit Sever <halitsever@protonmail.com>
2024-06-22 07:15:55 +0300

    feat: 🎸 update metatags

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Steam Deck Web App Generator | Generate Website to Web App for Steam Deck",
+  ogTitle: "Steam Deck Web App Generator",
+  description: "Steam deck web application generator, create shortcut scripts for your favorite websites to game mode",
+  ogDescription: "Steam deck web application generator, create shortcut scripts for your favorite websites to game mode",
+  ogImage: "https://steam-deck-web-app-generator.vercel.app/steam-deck.png",
+  twitterCard: "summary_large_image",
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
